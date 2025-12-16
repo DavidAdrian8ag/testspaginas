baa9272f078d07ae2ca5b1908a6f56d042d162e4
David8ag <krezitech@hotmail.com>
2025-12-16 10:11:22 -0600

    Jigsaw webresponsive

--- script.js
+++ script.js
@@ -3,26 +3,28 @@ const title = document.getElementById("title");
 
 title.innerHTML = "";
 
-text.split("").forEach((char) => {
-
+text.split("").forEach(char => {
    if (char === "\n") {
       title.appendChild(document.createElement("br"));
-   }
-   else if (char === " ") {
+   } else if (char === " ") {
       const space = document.createElement("span");
       space.innerHTML = "&nbsp;";
       title.appendChild(space);
-   }
-   else {
+   } else {
       const span = document.createElement("span");
       span.textContent = char;
       span.classList.add("letter");
       span.style.setProperty("--delay", Math.random() * 2 + "s");
       title.appendChild(span);
    }
-
 });
 
 document.getElementById("playBtn").addEventListener("click", () => {
-   window.location.href = "./safe.html";
+   window.location.href = "https://elenissmarriage.netlify.app/";
 });
+
+
+
+// document.getElementById("playBtn").addEventListener("click", () => {
+//    window.location.href = "./safe.html";
+// });
