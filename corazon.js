19eb205228f0e97718e020fc7fca53fe60fe8a70
David Adrián Ochoa <krezitech@hotmail.com>
2025-12-16 08:41:47 -0600

    corazones web responsive

---
+++ corazon.js
@@ -0,0 +1,14 @@
+function check() {
+   const checkboxes = document.querySelectorAll('form input[type="checkbox"]');
+   const allChecked = Array.from(checkboxes).every(cb => cb.checked);
+   const wrapper = document.querySelector('.wrapper');
+   const continuar = document.getElementById('continuar');
+
+   if (allChecked) {
+      wrapper.classList.add('throb');
+      continuar.classList.add('mostrar');
+   } else {
+      wrapper.classList.remove('throb');
+      continuar.classList.remove('mostrar');
+   }
+}
